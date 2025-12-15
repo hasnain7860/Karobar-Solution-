bf7004fa2b4814556095d7db8d5c5dbff4c5cf2c
hasnain7860 <httechnical7860@gmail.com>
2025-12-15 21:30:15 +0500

    add anaylatic

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -49,11 +49,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      {/* Step 2: GTM Script (Head Code) - Ye Next.js khud Head mein inject karega */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-WLTNLLQT');
+        `}
+      </Script>
+
       <body className={`${inter.className} bg-slate-50 text-slate-900`}>
+        {/* Step 3: GTM NoScript (Body Code) - Ye body ke bilkul shuru mein */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WLTNLLQT"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
   );
 }
-
 
